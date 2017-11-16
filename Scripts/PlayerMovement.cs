61079457b57b141d11fe058b0f877824cc71774d
tristan.jardon <tristan.jardon@gmail.com>
2017-11-16 19:18:47 +0100

    First commit

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        animator.SetFloat("horizontalSpeed", h);
+    }
+}
